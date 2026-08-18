84f267126b2ccd4a34d868b0124e3a63fe61f508
Kamlesh Pal <KamleshPal55@yahoo.com>
2026-08-18 14:02:02 -0700

    Updated BrowserNavigation.java

--- src/test/java/BrowserNavigation.java
+++ src/test/java/BrowserNavigation.java
@@ -2,12 +2,16 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BrowserNavigation {
 	public static WebDriver driver;
 
 	public static void setup() {
-		driver = new ChromeDriver();
+		//driver = new ChromeDriver();
+		//driver = new FirefoxDriver();
+		driver = new EdgeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
 	}
@@ -26,6 +30,10 @@ public class BrowserNavigation {
 		Thread.sleep(4);
 		driver.navigate().forward();
 		System.out.println(driver.getTitle()); //https://www.yahoo.com/
+		Thread.sleep(4);
+		driver.navigate().to("https://www.selenium.dev/");
+		Thread.sleep(4);
+		driver.navigate().refresh();
 	}
 	public static void tearDown() {
 		driver.quit();
